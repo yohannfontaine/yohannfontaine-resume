777e5371252fe02bd42c675695e5f82c251d1946
Yohann Fontaine <yohann.fontaine@gmail.com>
2023-09-03 22:30:53 +0200

    add portfolio images

--- components/PortfolioSection.js
+++ components/PortfolioSection.js
@@ -67,52 +67,31 @@ export default function PortfolioSection({ isActive }) {
                 <li>
                   <a
                     className="filter btn btn-sm btn-link"
-                    data-group="category_detailed"
-                    onClick={handleFilterKeyChange("standard")}
+                    data-group="category_pro"
+                    onClick={handleFilterKeyChange("pro")}
                   >
-                    Perso
+                    Professionel
                   </a>
                 </li>
                 <li>
                   <a
                     className="filter btn btn-sm btn-link"
-                    data-group="category_mockups"
+                    data-group="category_perso"
+                    onClick={handleFilterKeyChange("perso")}
                   >
-                    Mockups
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="filter btn btn-sm btn-link"
-                    data-group="category_soundcloud"
-                    onClick={handleFilterKeyChange("lbaudio")}
-                  >
-                    SoundCloud
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="filter btn btn-sm btn-link"
-                    data-group="category_vimeo-videos"
-                  >
-                    Vimeo Videos
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="filter btn btn-sm btn-link"
-                    data-group="category_youtube-videos"
-                  >
-                    YouTube Videos
+                    Personnel
                   </a>
                 </li>
               </ul>
 
               <div className="portfolio-grid three-columns">
-                <figure className="item perso">
+                <figure
+                  className="item pro"
+                  data-groups='["category_all", "category_pro"]'
+                >
                   <div className="portfolio-item-img">
                     <img
-                      src="img/portfolio/1.jpg"
+                      src="img/portfolio/ygamaa.png"
                       alt="ygamaa website"
                       title=""
                     />
@@ -123,111 +102,84 @@ export default function PortfolioSection({ isActive }) {
                     ></a>
                   </div>
 
-                  <i className="fa fa-volume-up"></i>
-                  <h4 className="name">SoundCloud Audio</h4>
-                  <span className="category">SoundCloud</span>
+                  <h4 className="name">Y-GAMAA Website</h4>
+                  <span className="category">Pro</span>
                 </figure>
 
                 <figure
-                  className="item standard"
-                  data-groups='["category_all", "category_detailed"]'
+                  className="item perso"
+                  data-groups='["category_all", "category_perso"]'
                 >
                   <div className="portfolio-item-img">
                     <img
-                      src="img/portfolio/2.jpg"
-                      alt="Media Project 2"
-                      title=""
-                    />
-                    <a href="portfolio-1.html" className="ajax-page-load"></a>
-                  </div>
-
-                  <i className="far fa-file-alt"></i>
-                  <h4 className="name">Detailed Project 2</h4>
-                  <span className="category">Detailed</span>
-                </figure>
-
-                <figure
-                  className="item lbvideo"
-                  data-groups='["category_all", "category_vimeo-videos"]'
-                >
-                  <div className="portfolio-item-img">
-                    <img
-                      src="img/portfolio/3.jpg"
-                      alt="Vimeo Video 1"
+                      src="img/portfolio/revealjs-effects.png"
+                      alt="Revealjs Effects"
                       title=""
                     />
                     <a
-                      href="https://player.vimeo.com/video/158284739"
+                      href="https://revealjs-cool-effects.web.app/"
+                      className="ajax-page-load"
+                    ></a>
+                  </div>
+                  <h4 className="name">Revealjs Effects</h4>
+                  <span className="category">Perso</span>
+                </figure>
+
+                <figure
+                  className="item pro"
+                  data-groups='["category_all", "category_pro"]'
+                >
+                  <div className="portfolio-item-img">
+                    <img
+                      src="img/portfolio/credit-agricole.png"
+                      alt="Credit Agricole"
+                      title=""
+                    />
+                    <a
+                      href="https://ca-paris.com/particuliers/"
                       className="lightbox mfp-iframe"
-                      title="Vimeo Video 1"
+                      title="Credit Agricole Website"
                     ></a>
                   </div>
 
-                  <i className="fas fa-video"></i>
-                  <h4 className="name">Vimeo Video 1</h4>
-                  <span className="category">Vimeo Videos</span>
+                  <h4 className="name">Crédit Agricole</h4>
+                  <span className="category">Pro</span>
                 </figure>
 
                 <figure
-                  className="item standard"
-                  data-groups='["category_all", "category_detailed"]'
+                  className="item pro"
+                  data-groups='["category_all", "category_pro"]'
                 >
                   <div className="portfolio-item-img">
-                    <img
-                      src="img/portfolio/4.jpg"
-                      alt="Media Project 1"
-                      title=""
-                    />
-                    <a href="portfolio-1.html" className="ajax-page-load"></a>
+                    <img src="img/portfolio/promod.png" alt="Promod" title="" />
+                    <a
+                      href="https://www.promod.fr"
+                      className="ajax-page-load"
+                    ></a>
                   </div>
 
-                  <i className="far fa-file-alt"></i>
-                  <h4 className="name">Detailed Project 1</h4>
-                  <span className="category">Detailed</span>
+                  <h4 className="name">Promod</h4>
+                  <span className="category">Pro</span>
                 </figure>
 
                 <figure
-                  className="item lbimage"
-                  data-groups='["category_all", "category_mockups"]'
+                  className="item perso"
+                  data-groups='["category_all", "category_perso"]'
                 >
                   <div className="portfolio-item-img">
                     <img
-                      src="img/portfolio/5.jpg"
-                      alt="Mockup Design 1"
+                      src="img/portfolio/yohann-resume.png"
+                      alt="Yohann Fontaine Resume"
                       title=""
                     />
                     <a
-                      className="lightbox"
-                      title="Mockup Design 1"
-                      href="img/portfolio/full/5.jpg"
+                      href="https://yohannfontaine-resume.web.app/"
+                      className="ajax-page-load"
                     ></a>
                   </div>
 
-                  <i className="far fa-image"></i>
-                  <h4 className="name">Mockup Design 1</h4>
-                  <span className="category">Mockups</span>
-                </figure>
-
-                <figure
-                  className="item lbvideo"
-                  data-groups='["category_all", "category_youtube-videos"]'
-                >
-                  <div className="portfolio-item-img">
-                    <img
-                      src="img/portfolio/6.jpg"
-                      alt="YouTube Video 1"
-                      title=""
-                    />
-                    <a
-                      href="https://www.youtube.com/embed/bg0gv2YpIok"
-                      className="lightbox mfp-iframe"
-                      title="YouTube Video 1"
-                    ></a>
-                  </div>
-
-                  <i className="fas fa-video"></i>
-                  <h4 className="name">YouTube Video 1</h4>
-                  <span className="category">YouTube Videos</span>
+                  <h4 className="name">My Resume</h4>
+                  <span className="category">Perso</span>
                 </figure>
               </div>
             </div>
